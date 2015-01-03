96d5b3f74c48056068ba121b8e70d3bd8d08c892
David <davidedwards122@googlemail.com>
2015-01-03 14:08:30 +0000

    test commit

---
+++ src/test/java/uk/co/weatherbys/endpoint/imp/TestEndPoint.java
@@ -0,0 +1,18 @@
+package uk.co.weatherbys.endpoint.imp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestEndPoint {
+	
+	
+	
+	@Test 
+	public void testStatus() {
+		
+		Assert.assertNotNull("test");
+			
+	}
+
+
+}
